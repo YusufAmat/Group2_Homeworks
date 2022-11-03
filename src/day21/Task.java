91a66a85d5907a8230696c0c6c8fc7f289e180e6
YusufAmat <hoccayusuf@gmail.com>
2022-11-03 08:58:37 +0100

    Die Hausaufgabe ist erledigt.(day21)

---
+++ src/day21/Task.java
@@ -0,0 +1,22 @@
+package day21;
+
+public class Task {
+    public static void main(String[] args) {
+        /*
+        {"Ahmet","Fizik","Şengül","Kimya","Hakan","Matematik","Feraye","Biyoloji",
+        "Cengiz","Felsefe","Salih","Elektronik","Sezen","Bilgisayar","Hasan","Fizik","Sezgin","Biyoloji" };
+        */
+
+        /*Yukarıdaki dizi verildiğine göre;
+        Adı Bölümü
+        Ahmet Fizik
+        biçiminde ekran çıktısı veren bir Java programı yazınız.*/
+
+        String[] data = {"Ahmet", "Fizik", "Şengül", "Kimya", "Hakan", "Matematik", "Feraye", "Biyoloji", "Cengiz",
+                "Felsefe", "Salih", "Elektronik", "Sezen", "Bilgisayar", "Hasan", "Fizik", "Sezgin", "Biyoloji"};
+        for (int i = 0; i < data.length; i += 2) {
+            System.out.println(data[i] + "\t-->  " + data[i + 1]);
+        }
+
+    }
+}
