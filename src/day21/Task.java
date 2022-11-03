2f9b33e9c111693cc0c60eb0571f71a86f115c33
YusufAmat <hoccayusuf@gmail.com>
2022-11-03 09:09:06 +0100

    Die Hausaufgabe ist erledigt.(day21)

--- src/day21/Task.java
+++ src/day21/Task.java
@@ -14,6 +14,8 @@ public class Task {
 
         String[] data = {"Ahmet", "Fizik", "Şengül", "Kimya", "Hakan", "Matematik", "Feraye", "Biyoloji", "Cengiz",
                 "Felsefe", "Salih", "Elektronik", "Sezen", "Bilgisayar", "Hasan", "Fizik", "Sezgin", "Biyoloji"};
+        System.out.println("ADI\t\t||   BÖLÜMÜ");
+        System.out.println("------------------");
         for (int i = 0; i < data.length; i += 2) {
             System.out.println(data[i] + "\t-->  " + data[i + 1]);
         }
